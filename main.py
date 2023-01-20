afb8c3f9541a15ea42fad786f8f3addf0ea12876
unknown <hamdmrady63@gmail.com>
2023-01-20 15:43:24 +0430

    tik tok, selenium

---
+++ main.py
@@ -0,0 +1,12 @@
+class TikTok:
+    def __init__(self):
+        pass
+    def scraper(self):
+        pass
+    def exportar(self):
+        pass
+    def run(self):
+        pass
+if __name__ == "__main__":
+    bot = TikTok()
+    bot.run()
